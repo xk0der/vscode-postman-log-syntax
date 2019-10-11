f85883bfef0635ba8c75945cd9193b2e8f2b0abc
xk0der <amit@xkoder.com>
2019-10-12 02:17:13 +1300

    Added auto detection of Postman logs instead of treating every /log file as Postman log

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,4 @@
 # Change Log
 
 ## [0.0.1]
-
-- Initial release
-- When installed and enabled treats all ".log" files as Postman logs and provide context aware highlighting to easily consume the log.
+- [xk0der] Initial release
---
+++ extension.js
@@ -0,0 +1,44 @@
+const vscode = require('vscode');
+
+
+function getActiveDoc() {
+    try {
+        return vscode.window.activeTextEditor.document;
+    } catch(exp) {}
+
+    return null;
+}
+
+function handleDocOpen(activeDoc) {
+    try {
+        if(activeDoc && activeDoc.languageId !== "postman_log") {
+            var data = activeDoc.getText();
+            var lines = data.split("\n");
+            var isPostmanLog = 0;
+            var maxLines = (lines.length > 5 ? 5 : lines.length);
+            for(var idx = 0; idx < maxLines; idx++) {
+                if(lines[idx].match(/^\[[0-9]+\]\[[0-9]{13,13}\]\[[a-zA-Z]+\]\[[a-z]+\]\[[\[{"].*["}\]]\]$/)) {
+                    isPostmanLog++;
+                }
+            }
+            if(isPostmanLog === maxLines ) {
+                vscode.languages.setTextDocumentLanguage(activeDoc, "postman_log");
+            } 
+        }
+    } catch(exp) {
+        console.log(exp);
+    }
+}
+
+function activate(context) {
+    context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(handleDocOpen));
+    setTimeout( function () {
+        handleDocOpen(getActiveDoc());
+    }, 1000);
+}
+
+function deactivate() {
+}
+
+exports.activate = activate;
+exports.deactivate = deactivate;
